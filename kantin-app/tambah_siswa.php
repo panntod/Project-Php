c7fca1d8292b81fb904d9539ec6a0cd220ab76e1
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-08-31 10:12:25 +0700

    feature: add include method

--- kantin-app/tambah_siswa.php
+++ kantin-app/tambah_siswa.php
@@ -4,26 +4,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Login Page</title>
-
-        <!-- Favicons -->
-        <link href="assets/img/favicon.png" rel="icon">
-        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-        <!-- Google Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
-
-        <!-- Vendor CSS Files -->
-        <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-        <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-        <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-        <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-        <!-- Template Main CSS File -->
-        <link href="assets/css/main.css" rel="stylesheet">
+        <title>Regristate Page</title>
+        <?php include "style.php"; ?>
     </head>
 
     <body>
@@ -59,7 +41,7 @@
                         <input type="email" class="form-control" name="email" id="email" style="height: 50px; border-radius: 10px;" placeholder="Masukan Email" required>
                     </div>
                     <div class="form-group mt-3">
-                        <input type="password" class="form-control" name="password" id="password" style="height: 50px; border-radius: 10px;" placeholder="Masukan Password" required>
+                        <input type="password" class="form-control" name="password" id="password" style="height: 50px; border-radius: 10px;" placeholder="Masukan Password Baru" required>
                 </div>
 
                 <div class="my-3">
@@ -68,7 +50,7 @@
                     <div class="sent-message">Your regristate is succesfully. Thank you!</div>
                 </div>
                 <div class="text-center">
-                    <button type="submit" onclick="validateAndPindah()">Tambahkan</button>
+                    <button type="submit" onclick="validateAndPindah()">Update</button>
                 </div>
                 </form>
             </div><!-- End Contact Form -->
@@ -86,7 +68,7 @@
                 var email = document.getElementById("email").value;
                 var password = document.getElementById("password").value;
 
-                if (nama === "" || kelas === "" || email === "" || password === "") {
+                if (nama === "" || kelas === "" || email === "") {
                     var errorMessage = document.querySelector(".error-message");
                     errorMessage.textContent = "Please fill in all fields.";
                     errorMessage.style.backgroundColor = "red";
@@ -98,15 +80,6 @@
                 }
             }
         </script>
-        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="assets/vendor/aos/aos.js"></script>
-        <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-        <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-        <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-        <script src="assets/vendor/php-email-form/validate.js"></script>
-
-    <!-- Template Main JS File -->
-    <script src="assets/js/main.js"></script> 
+        <?php include "scripts.php"; ?>
     </body>
 </html>
--- kantin-app/ubah_siswa.php
+++ kantin-app/ubah_siswa.php
@@ -1,35 +1,91 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <title></title>
-</head>
-<body class="container">
-    <?php 
-    include "server.php";
-    $qry_get_siswa=mysqli_query($conn,"select * from siswa where id = '{$_GET['id']}'");
-    $dt_siswa=mysqli_fetch_array($qry_get_siswa);
-    ?>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Update Page</title>
+        <?php include 'style.php'?>
+    </head>
 
-    <h3>Edit Siswa</h3>
-    <form action="proses_ubah_siswa.php" method="post">
-        <input type="hidden" name="id" value="<?= $dt_siswa['id'] ?>">
+    <body>
+        <!-- ======= Contact Section ======= -->
+        <section id="contact" class="contact">
+        <div class="container" data-aos="fade-up">
 
-        <br>nama siswa : 
-        <input type="text" name="nama" value="<?=$dt_siswa['nama']?>" class="form-control">
-        
-        <br>Kelas: 
-        <input type="text" name="kelas" value="<?=$dt_siswa['kelas']?>" class="form-control">
-        
-        <br>Email : 
-        <input type="text" name="email" value="<?=$dt_siswa['email']?>" class="form-control">
-        
-        <br>Password : 
-        <input type="password" name="password" value="" class="form-control">
-        <input type="submit" name="simpan" value="Ubah Siswa" class="btn btn-primary" style="margin-top: 18px">
-    </form>
+            <div class="section-header">
+            <h2>Update Page</h2>
+            </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-</body>
+            <div class="row gx-lg-0 gy-4">
+
+            <div class="col-lg-4">
+
+                <div class="info-container d-flex flex-column align-items-center justify-content-center">
+                <img src="assets/img/hero-img.svg" class="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="100">
+                </div>
+
+            </div>
+
+            <?php 
+                include "server.php";
+                $qry_get_siswa=mysqli_query($conn,"select * from siswa where id = '{$_GET['id']}'");
+                $dt_siswa=mysqli_fetch_array($qry_get_siswa);
+            ?>
+
+            <div class="col-lg-8">
+                <form action="proses_ubah_siswa.php" method="post" class="php-email-form">
+                <input type="hidden" name="id" value="<?= $dt_siswa['id'] ?>">
+                <div class="row">
+                    <div class="form-group mt-3">
+                        <input type="text" name="nama" value="<?=$dt_siswa['nama']?>" class="form-control" style="height: 50px; border-radius: 10px;" placeholder="Masukan Nama" required>
+                    </div>
+                    <div class="form-group mt-3">
+                        <input type="text" class="form-control" value="<?=$dt_siswa['kelas']?>" name="kelas" style="height: 50px; border-radius: 10px;" placeholder="Masukan Kelas" required>
+                        </div>
+                    </div>
+                    <div class="form-group mt-3">
+                        <input type="email" class="form-control" value="<?=$dt_siswa['email']?>" name="email" id="email" style="height: 50px; border-radius: 10px;" placeholder="Masukan Email" required>
+                    </div>
+                    <div class="form-group mt-3">
+                        <input type="password" class="form-control" name="password" id="password" style="height: 50px; border-radius: 10px;" placeholder="Masukan Password" required>
+                </div>
+
+                <div class="my-3">
+                    <div class="loading">Loading</div>
+                    <div class="error-message" style="background-color: green; border-radius:11px"></div>
+                    <div class="sent-message">Your regristate is succesfully. Thank you!</div>
+                </div>
+                <div class="text-center">
+                    <button type="submit" onclick="validateAndPindah()">Tambahkan</button>
+                </div>
+                </form>
+            </div><!-- End Contact Form -->
+
+            </div>
+
+        </div>
+        </section><!-- End Contact Section -->
+        </div>
+
+        <script>
+            function validateAndPindah() {
+                var nama = document.getElementsByName("nama")[0].value;
+                var kelas = document.getElementsByName("kelas")[0].value;
+                var email = document.getElementById("email").value;
+                var password = document.getElementById("password").value;
+
+                if (nama === "" || kelas === "" || email === "" || password === "") {
+                    var errorMessage = document.querySelector(".error-message");
+                    errorMessage.textContent = "Please fill in all fields.";
+                    errorMessage.style.backgroundColor = "red";
+                    errorMessage.style.borderRadius = "11px";
+                    errorMessage.style.display = "block";
+                } else {
+                    // Semua field terisi, pindah ke halaman berikutnya
+                    window.location.href = "tampil_siswa.php";
+                }
+            }
+        </script>
+         <?php include "scripts.php"; ?>
+    </body>
 </html>
-
