7aac555e4f257d08ba6b01efc56141cc162d8e4a
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-09-11 12:25:56 +0700

    feature: add navbar icon and add saldo

---
+++ kantin-app/proses_tambah_saldo.php
@@ -0,0 +1,21 @@
+<?php
+session_start(); 
+if ($_POST) {
+    $id_siswa = $_SESSION['id_siswa'];
+    $saldo = $_POST['saldo'];
+    if (empty($saldo)) {
+        echo "<script>alert('Saldo tidak boleh kosong');location.href='tambah_saldo.php';</script>";
+    } else {
+        include "server.php"; 
+        $query = "UPDATE siswa SET saldo = saldo + $saldo WHERE id_siswa = $id_siswa";
+
+        $insert = mysqli_query($conn, $query) or die(mysqli_error($conn));
+
+        if ($insert) {
+            echo "<script>alert('Sukses menambahkan saldo');location.href='tambah_saldo.php';</script>";
+        } else {
+            echo "<script>alert('Gagal menambahkan saldo');location.href='tambah_saldo.php';</script>";
+        }
+    }
+}
+?>
