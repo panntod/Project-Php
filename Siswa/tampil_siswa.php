f9870ed9d7a8d7abe8dbc7c62d1801fb466d017c
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-10-10 12:43:36 +0700

    [style] added boostrap icon

--- Siswa/tampil_siswa.php
+++ Siswa/tampil_siswa.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <title></title>
 </head>
-<body>
+
+<body class="container">
     <div style="padding: 20px; display: flex; justify-content: space-between;">
         <h3>Tampil Siswa</h3>
         <div style="display: flex; gap: 10px;">
@@ -28,34 +32,55 @@
             </tr>
         </thead>
         <tbody>
-            <?php 
+            <?php
             include "connect.php";
-            $qry_siswa=mysqli_query($conn,"select * from siswa join kelas on kelas.id_kelas=siswa.id_kelas");
-            $no=0;
-            while($data_siswa=mysqli_fetch_array($qry_siswa)){
-            $no++;?>
-            <tr>
-                <td><?=$no?></td>
-                <td><?=$data_siswa['nama_siswa']?></td>
-                <td><?=$data_siswa['tanggal_lahir']?></td>
-                <td><?=$data_siswa['alamat']?></td>
-                <td><?=$data_siswa['gender']?></td> 
-                <td><?=$data_siswa['username']?></td> 
-                <td><?=$data_siswa['nama_kelas']?></td> 
-                <td><?=$data_siswa['id_siswa']?></td>
-                <td>
-                    <a href="ubah_siswa.php?id_siswa=<?=$data_siswa['id_siswa']?>" class="btn btn-success">Ubah</a>
-                     | 
-                    <a href="hapus.php?id_siswa=<?=$data_siswa['id_siswa']?>" onclick="return confirm('Apakah anda yakin menghapus data ini?')" class="btn btn-danger">Hapus</a>
-                </td>
-            </tr>
-            <?php 
+            $qry_siswa = mysqli_query($conn, "select * from siswa join kelas on kelas.id_kelas=siswa.id_kelas");
+            $no = 0;
+            while ($data_siswa = mysqli_fetch_array($qry_siswa)) {
+                $no++; ?>
+                <tr>
+                    <td>
+                        <?= $no ?>
+                    </td>
+                    <td>
+                        <?= $data_siswa['nama_siswa'] ?>
+                    </td>
+                    <td>
+                        <?= $data_siswa['tanggal_lahir'] ?>
+                    </td>
+                    <td>
+                        <?= $data_siswa['alamat'] ?>
+                    </td>
+                    <td>
+                        <?= $data_siswa['gender'] ?>
+                    </td>
+                    <td>
+                        <?= $data_siswa['username'] ?>
+                    </td>
+                    <td>
+                        <?= $data_siswa['nama_kelas'] ?>
+                    </td>
+                    <td>
+                        <?= $data_siswa['id_siswa'] ?>
+                    </td>
+                    <td>
+                        <a href="ubah_siswa.php?id_siswa=<?= $data_siswa['id_siswa'] ?>" class="btn btn-warning text-white"><i
+                                class="bi bi-pencil-square"></i></a>
+                        <a href="hapus.php?id_siswa=<?= $data_siswa['id_siswa'] ?>"
+                            onclick="return confirm('Apakah anda yakin menghapus data ini?')" class="btn btn-danger"><i
+                                class="bi bi-trash"></i></a>
+                    </td>
+                </tr>
+                <?php
             }
             ?>
         </tbody>
-        
+
     </table>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
