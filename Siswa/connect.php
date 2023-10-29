5b4c0cc86ec85306fbade234a0895abb025767bd
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-10-30 04:30:02 +0700

    [refactor] update mysql

--- Siswa/connect.php
+++ Siswa/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect('localhost','root','','belajar_laravel');
+$conn=mysqli_connect('localhost','root','','perpustakaan');
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
